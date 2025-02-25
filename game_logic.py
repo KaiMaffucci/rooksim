df548704c60bb2098a66d96701f6084f1a51993b
KaiMaffucci <kai.maffucci@gmail.com>
2025-02-24 23:02:47 -0500

    Added quick skeleton code for characters. Real code will come soon

--- game_logic.py
+++ game_logic.py
@@ -39,8 +39,6 @@ class Game:
         self.winning_mid = 0
         current_trick = []
 
-    # TODO: need methods for gameplay
-
     # deals starting hand
     def deal():
         pass
@@ -63,6 +61,7 @@ class Player:
 
     def __init__(self):
 
+        self.score = 0          # everyone starts at 0 points
         self.hand = []          # empty hand, deck hasnt been dealt yet
         self.taken = []         # no tricks taken yet
         self.current_bid = 0    # no bid started yet
@@ -73,8 +72,53 @@ class Player:
         pass
 
     # play the card in a trick
-    def play():
+    def card():
         pass
 
 
-# different characters will be their own child classes
+# Hideous Hog: aggressive but skilled
+class HH(Player):
+
+    # player methods for playing the game
+    def bid():
+        pass
+
+    # play the card in a trick
+    def card():
+        pass
+
+
+# Papa Greek: moderate player, tries to be optimal but this often makes him unlucky
+class Greek(Player):
+
+    # player methods for playing the game
+    def bid():
+        pass
+
+    # play the card in a trick
+    def card():
+        pass
+
+
+# Karapet: cautious, not a risk taker, looks to minimize loss moreso
+class Karapet(Player):
+
+    # player methods for playing the game
+    def bid():
+        pass
+
+    # play the card in a trick
+    def card():
+        pass
+
+
+# Rueful Rabbit: wildcard, plays bad but still wins (last to be programmed)
+class RR(Player):
+
+    # player methods for playing the game
+    def bid():
+        pass
+
+    # play the card in a trick
+    def card():
+        pass
