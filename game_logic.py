bc2ce4a1e374b39b7cf764914d84c136d4ab2d3c
KaiMaffucci <kai.maffucci@gmail.com>
2025-02-26 21:43:35 -0500

    Wrote a bunch of code

--- game_logic.py
+++ game_logic.py
@@ -24,7 +24,14 @@ deck const?
 make sure to output stuff to file
 
 calculate how many holes are in a hand -> part of bidding
+
+may need benchmarking
+
 """
+
+
+import random
+
 
 # game class
 class Game:
@@ -32,28 +39,122 @@ class Game:
     def __init__(self):
 
         # later will specify certain characters
-        self.player1 = Player()
-        self.player2 = Player()
-        self.player3 = Player()
-        self.player4 = Player()
-        self.winning_mid = 0
-        current_trick = []
+        # assume 1 & 3 will be partners, 2 & 4 will be partners
+        # TODO: randomly assign them one of four unique characters
+        self.p1 = Player()
+        self.p2 = Player()
+        self.p3 = Player()
+        self.p4 = Player()
 
-    # deals starting hand
-    def deal():
-        pass
+        self.nest = [] # cards in the nest/kiddy
+        self.current_trick = [] # cards in the current trick
+        self.leading_suit = '' # suit of the first card played in a trick
+        self.leading_player = 0 # 1-4, which player will lead the next trick
+        self.winning_bid = 0 # the highest bid made by a player
+        self.trump = '' # the trump suit for the round
 
-    # make players bid
-    def all_bid():
-        pass
+
+    # deals starting hand and assigns partners
+    # TODO: write test cases
+    def deal(self):
+        
+        # Rook deck
+        # has 57-16=41 cards, 4 suits of 14 cards each, 1 rook
+        # suits are red, green, yellow, black
+        deck = []
+        for i in range(5, 15):
+            deck.append('R' + str(i))
+            deck.append('G' + str(i))
+            deck.append('Y' + str(i))
+            deck.append('B' + str(i))
+        deck.append('X')
+
+        # shuffle deck
+        random.shuffle(deck)
+
+        # deal cards to each player
+        for i in range(0, 9):
+            self.p1.hand.append(deck.pop())
+            self.p2.hand.append(deck.pop())
+            self.p3.hand.append(deck.pop())
+            self.p4.hand.append(deck.pop())
+        
+        # deal cards to the nest
+        for i in range(0, 5):
+            self.nest.append(deck.pop())
+        
+        # TODO: move this?
+        # determine partners
+        # p1 and p3 are partners
+        self.p1.partner = 3
+        self.p3.partner = 1
+        # p2 and p4 are partners
+        self.p2.partner = 4
+        self.p4.partner = 2
+
+    # make players bid (TODO)
+    def all_bid(self):
+        
+        # bid loop until all players have passed
+        while self.p1.passing == False or self.p2.passing == False or self.p3.passing == False or self.p4.passing == False:
+            # player 1 bids
+            self.p1.bid()
+            # player 2 bids
+            self.p2.bid()
+            # player 3 bids
+            self.p3.bid()
+            # player 4 bids
+            self.p4.bid()
+        
+        # determine the winning bid
+        self.winning_bid = max(self.p1.current_bid, self.p2.current_bid, self.p3.current_bid, self.p4.current_bid)
+
+        # TODO: determine the trump suit
+
+        # TODO: need method(?) for nest setup, before round starts
 
     # play a single trick
-    def play_trick():
+    def play_trick(self):
         pass
 
+    # play a single round
+    def play_round(self):
+        
+        # deal cards to each player
+        self.deal()
+
+        # players bid
+        self.all_bid()
+
+        # play tricks until all players are out of cards
+        while len(self.player1.hand) > 0:
+            self.play_trick()
+        
+        # TODO: determine how many points are in each player's taken tricks
+        
+        # TODO: if the highest bid was met, add the points to the team's score
+        # if not, subtract the points from the team's score
+
+
     # run whole game
-    def run():
-        pass
+    def run(self):
+        
+        # while none of the players' scores are 300 or more, play another round
+        while self.p1.score < 300 and self.p2.score < 300 and self.p3.score < 300 and self.p4.score < 300:
+            self.play_round()
+        
+        # determine the winner
+        # TODO: work on better data storage/output
+        if self.p1.score >= 300:
+            print("Player 1 wins!")
+        elif self.p2.score >= 300:
+            print("Player 2 wins!")
+        elif self.p3.score >= 300:
+            print("Player 3 wins!")
+        elif self.p4.score >= 300:
+            print("Player 4 wins!")
+        else:
+            print("Error: no winner found")
 
 
 # player class
@@ -64,8 +165,10 @@ class Player:
         self.score = 0          # everyone starts at 0 points
         self.hand = []          # empty hand, deck hasnt been dealt yet
         self.taken = []         # no tricks taken yet
+        self.pts_taken = 0      # no points from tricks calculated yet
         self.current_bid = 0    # no bid started yet
         self.partner = 0        # partner undeclared
+        self.passing = False    # whether the player is passing on bidding yet (for bidding phase, starts as false)
 
     # player methods for playing the game
     def bid():
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from game_logic import *
 
 def main():
-    pass
+
+    
+    game = Game()
+
+    # test code
+    game.deal()
+    print(game.p1.hand)
+    print(game.p2.hand)
+    print(game.p3.hand)
+    print(game.p4.hand)
+    print(game.nest)
+
+main()
